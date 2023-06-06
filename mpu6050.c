f6d40907209bd08707d449d9e131241df3cb9049
qvietp <133985766+qvietp@users.noreply.github.com>
2023-06-06 19:54:11 +0700

    Update mpu6050.c

--- mpu6050.c
+++ mpu6050.c
@@ -3,11 +3,11 @@
 
 void MPU6050_Init(void)
 {
-    // KÌch ho?t ngu?n MPU6050
+    // K√≠ch hoat nguon MPU6050
     uint8_t turn_on[] = {0x6B, 0x00};
     I2C_Write(MPU6050_ADDR, turn_on, sizeof(turn_on));
 
-    // C?u hÏnh gi· tr? scale cho gia t?c
+    // Cau h√¨nh gi√° tri scale cho gia toc
     uint8_t set_scale[] = {0x1C, 0x00};
     I2C_Write(MPU6050_ADDR, set_scale, sizeof(set_scale));
 }
@@ -16,11 +16,11 @@ void MPU6050_GetAcceleration(float* acceleration)
 {
     uint8_t buffer[6];
 
-    // –?c gi· tr? d? d?c t? MPU6050
+    // √êoc gi√° tri do duoc tu MPU6050
     uint8_t read_addr = 0x3B;
     I2C_Read(MPU6050_ADDR, &read_addr, 1, buffer, 6);
 
-    // Chuy?n d?i gi· tr? d? d?c th‡nh don v? m/s^2
+    // Chuyen doi gi√° tri do duoc th√†nh don vi m/s^2
     acceleration[0] = (float)((int16_t)((buffer[0] << 8) | buffer[1])) / MPU6050_ACCEL_SCALE_2G * 9.81;
     acceleration[1] = (float)((int16_t)((buffer[2] << 8) | buffer[3])) / MPU6050_ACCEL_SCALE_2G * 9.81;
     acceleration[2] = (float)((int16_t)((buffer[4] << 8) | buffer[5])) / MPU6050_ACCEL_SCALE_2G * 9.81;
