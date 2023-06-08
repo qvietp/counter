4989c1ed1d84fca0f31e41952a6a0abcc42d1fbd
qvietp <133985766+qvietp@users.noreply.github.com>
2023-06-08 08:52:54 +0700

    Update lcd_i2c.h

--- lcd_i2c.h
+++ lcd_i2c.h
@@ -1,11 +1,33 @@
-#ifndef LCD_I2C_H_
-#define LCD_I2C_H_
+#ifndef I2C_LCD_H_
+#define I2C_LCD_H_
 
 #include "stm32f10x.h"
+#include "i2c.h"
+#include <stdio.h>
 
-#define LCD_ADDR 0x3F
+#define I2C_LCD_ADDR 0x4E
 
-void LCD_I2C_Init(void);
-void LCD_I2C_DisplayNumber(uint16_t number);
+extern void Delay_Ms(uint32_t u32DelayInMs);
 
-#endif /* LCD_I2C_H_ */
+#define I2C_LCD_Delay_Ms(u16DelayMs) Delay_Ms(u16DelayMs)
+
+#define LCD_EN 2
+#define LCD_RW 1
+#define LCD_RS 0
+#define LCD_D4 4
+#define LCD_D5 5
+#define LCD_D6 6
+#define LCD_D7 7
+#define LCD_BL 3
+
+void I2C_LCD_Setup(void);
+void I2C_LCD_Init(void);
+void I2C_LCD_Puts(char *szStr);
+void I2C_LCD_Clear(void);
+void I2C_LCD_NewLine(void);
+void I2C_LCD_BackLight(uint8_t u8BackLight);
+static void I2C_LCD_Write_4bit(uint8_t u8Data);
+static void I2C_LCD_FlushVal(void);
+static void I2C_LCD_WriteCmd(uint8_t u8Cmd);
+
+#endif
